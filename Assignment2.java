b4ae8b5e3d90be6344cd427e3e2d8e1abf9ec77b
zakattack9 <zakattack26111@gmail.com>
2018-09-14 08:07:38 -0700

    adding initial projects/labs

---
+++ Assignment2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Assignment2 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    String originalStr;
+    String searchStr;
+
+    System.out.println("Type in an input phrase");
+    originalStr = scan.nextLine();
+
+    System.out.println("Type in a search phrase");
+    searchStr = scan.nextLine();
+
+    System.out.println("Would you like to consider spaces? (1 (Yes), 0 (No))");
+    int spaces = Integer.parseInt(scan.nextLine());
+
+    System.out.println("Would you like to consider uppercase and lowercase letters? (1 (Yes), 0 (No))");
+    int upperLower = Integer.parseInt(scan.nextLine());
+
+    if (spaces == 0) {
+      originalStr = originalStr.replace(" ", "");
+      searchStr = searchStr.replace(" ", "");
+
+    } if (upperLower == 0) {
+      originalStr = originalStr.toUpperCase();
+      searchStr = searchStr.toUpperCase();
+
+    } if (originalStr.equals(searchStr)) { 
+      System.out.println("Search phrase is equal to entire input phrase");
+
+    } else if (!originalStr.contains(searchStr)) {
+      System.out.println("Search phrase is not in input phrase");
+
+    } else {
+      if (originalStr.startsWith(searchStr)) {
+        System.out.println("Search phrase is at the start of input phrase");
+      } else if (originalStr.endsWith(searchStr)) {
+        System.out.println("Search phrase is at the end of input phrase");
+      } else {
+        System.out.println("Search phrase starts at index " + originalStr.indexOf(searchStr) + " and ends at index " + (originalStr.indexOf(searchStr) + searchStr.length() - 1));
+      }
+    }
+    // System.out.println(searchStr + originalStr);
+  }
+}
---
+++ Lab3.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Lab3 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Input a score");
+    double score = scan.nextDouble();
+
+    if (score >= 90) {
+      System.out.println("Graded as an A");
+    } else if (score < 90 && score >= 80) {
+      System.out.println("Graded as an B");
+    } else if (score < 80 && score >= 60) {
+      System.out.println("Graded as an C");
+    } else {
+      System.out.println("Graded as an D");
+    }
+
+    System.out.println("Input another score");
+    int score2 = scan.nextInt();
+    
+    while (score2 >= 0) {
+      switch (score2 / 10) {
+        case 9:
+          System.out.println("A Grade");
+          break;
+        case 8:
+          System.out.println("B Grade");
+          break;
+        case 7: //!!! case meant to flow to following case
+        case 6:
+          System.out.println("C Grade");
+          break;
+        default:
+          System.out.println("D Grade");
+          break;
+      }
+      score2 = scan.nextInt();
+    }
+    System.out.println("Please enter a valid score");
+
+    scan.close();
+  }
+}
